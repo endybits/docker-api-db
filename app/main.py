673984e83c33520c961c97339088b7612d6ae8c0
Endy B <endyb.dev@gmail.com>
2023-09-16 14:29:03 +1000

    Connect to db

--- app/main.py
+++ app/main.py
@@ -2,11 +2,14 @@ import os
 
 from fastapi import FastAPI
 
+from app.db import db_connection
 app = FastAPI()
 
 @app.get("/")
 def hello():
+    print(db_connection())
     return {
         "hello": "this is a container service",
-        "from hostname": f"{os.environ.get('HOSTNAME', 'DEFAULT_ENV')}"
+        "from hostname": f"{os.environ.get('HOSTNAME', 'DEFAULT_ENV')}",
+        "db connection": f"{db_connection()}"
     }
